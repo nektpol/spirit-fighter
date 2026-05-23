0bcb3792cef54a79a7c02c64ba341c6040b2d4c5
nektpol <36765011+nektpol@users.noreply.github.com>
2026-05-23 12:24:54 +0300

    create spirit data

---
+++ scripts/data_types/SpiritData.gd
@@ -0,0 +1,18 @@
+extends Resource
+class_name SpiritData
+
+@export var id: String
+@export var spirit_name: String
+@export var spirit_class: SpiritTypes.Class
+
+@export var element: String
+
+@export var max_hp: int = 100
+@export var attack: int = 10
+@export var defense: int = 5
+@export var speed: int = 5
+
+@export var abilities: Array[Resource] = []
+
+@export var icon: Texture2D
+@export var portrait: Texture2D
